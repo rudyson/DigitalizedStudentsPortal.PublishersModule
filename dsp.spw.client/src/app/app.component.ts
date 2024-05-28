43b30a82dd3028199bfb83cf971729ae87280b70
Ruslan Diadiushkin <contact@xnrudyson.anonaddy.me>
2024-05-28 22:49:11 +0300

    Updated angular version, installed primeng, added template layout

--- dsp.spw.client/src/app/app.component.ts
+++ dsp.spw.client/src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  tabMenuItems: MenuItem[] | undefined;
+  sideMenuItems: MenuItem[] | undefined;
+
   title = 'dsp.spw.client';
+  ngOnInit() {
+    this.tabMenuItems = [
+      { label: 'Dashboard', icon: 'pi pi-home' },
+      { label: 'Transactions', icon: 'pi pi-chart-line' },
+      { label: 'Products', icon: 'pi pi-list' },
+      { label: 'Messages', icon: 'pi pi-inbox' },
+    ];
+    this.sideMenuItems = [
+      {
+        separator: true,
+      },
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            shortcut: '⌘+N',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+            shortcut: '⌘+S',
+          },
+        ],
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+            shortcut: '⌘+O',
+          },
+          {
+            label: 'Messages',
+            icon: 'pi pi-inbox',
+            badge: '2',
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            shortcut: '⌘+Q',
+          },
+        ],
+      },
+      {
+        separator: true,
+      },
+    ];
+  }
 }
--- dsp.spw.client/src/app/app.module.ts
+++ dsp.spw.client/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TabMenuModule,
+    MenuModule,
+    BadgeModule,
+    RippleModule,
+    AvatarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
